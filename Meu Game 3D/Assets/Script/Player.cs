b65b04e9b8b21ca0674ea19eed96f409519403f9
Kemillyrochaa <r.kemilly@escolar.ifrn.edu.br>
2024-08-27 14:22:11 -0300

    Player

---
+++ Meu Game 3D/Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Searcher;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    Rigidbody rb;
+
+    public int forcaPulo = 10;
+
+    public bool noChao = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb); 
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        if (transform.position.y < -5);
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
